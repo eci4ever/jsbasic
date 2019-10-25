d6447021f8736753bab506c22ded4f06a8442437
eci4ever <eci4ever@gmail.com>
2019-10-25 21:19:53 +0800

    Learn

--- solo/01.js
+++ solo/01.js
@@ -1,6 +1,38 @@
-var text1 = "Hello World!";       // String
-var text2 = text1.toUpperCase();  // text2 is text1 converted to upper
-console.log(text2);
+
+var arr = [5,7,2,6,3];
+for (var c=1; c <= 5; c +=2);
+var x = arr[3];
+x = (x%4);
+x *= 2;
+c = (c%x);
+console.log(arr[c]);
+
+// var i=0;
+// var numbers = new Array();
+// function kira(val) {
+// numbers[i] = val;
+// i++;
+// }
+//numbers = [1,2,3];
+// kira(5);
+// console.log(numbers[i]);
+// var x = 0;
+// var y = 0;
+// var arr = [4,7,2];
+// arr[x] = x++;
+// x = x++;
+// y = ++y;
+
+// console.log(x + y);
+
+// var x = 0;
+// var arr = [4,7,2];
+// arr[x] = x++;
+// console.log(arr[x]);
+
+// var text1 = "Hello World!";       // String
+// var text2 = text1.toUpperCase();  // text2 is text1 converted to upper
+// console.log(text2);
 
 // var str = "Please locate where 'locate' occurs!";
 // var pos = str.indexOf("locate");
