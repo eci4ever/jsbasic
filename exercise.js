0aa2b0045ee14cad9d4b0791368d305e895d93b7
eci4ever <eci4ever@gmail.com>
2019-10-28 22:21:32 +0800

    exercise

---
+++ exercise.js
@@ -0,0 +1,13 @@
+
+
+// var person = { firstName:"Nik Mohd Fairus ", lastName: "Ismail", age : 40 }
+
+// console.log(person.firstName + person.lastName + " berumur " + person.age);
+// var x = 20;
+// console.log(x == 20);
+
+// function io1 () {
+//     let name = "Ladious Jones.";
+//     console.log(name.lastIndexOf("."));
+// }
+// io1();
