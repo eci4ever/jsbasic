24e869e9633468a0fdaec284bce75b277dbd950f
eci4ever <eci4ever@gmail.com>
2019-11-02 22:25:41 +0800

    exercise

--- exercise.js
+++ exercise.js
@@ -1,5 +1,25 @@
 
+var x = "John";             
+var y = new String("John");
 
+console.log(x == y);
+console.log(x === y);
+
+// var str = "I like to drive a \"Proton\" car";
+// console.log(str);
+
+// var person = {
+//     firstName: "Eci",
+//     lastName: "Ismail",
+//     age: 50,
+//     eyeColor: "black",
+//     fullname: function() { return this.firstName + " " + this.lastName; }
+//   };
+// console.log(person.fullname());
+// console.log(typeof(person));
+  
+// var z = 5 << 3;
+// console.log(z);
 // var person = { firstName:"Nik Mohd Fairus ", lastName: "Ismail", age : 40 }
 
 // console.log(person.firstName + person.lastName + " berumur " + person.age);
